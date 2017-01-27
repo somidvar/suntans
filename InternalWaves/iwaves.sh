846dcf48ebf0ea263b2ba9e332db2897f52b2fbd
Sorush Omidvar <sorush.omidvar@gmail.com>
2017-01-27 14:08:49 -0500

    makefile, iwaves.sh and iwaves_sub.sh are updated

--- InternalWaves/iwaves.sh
+++ InternalWaves/iwaves.sh
@@ -5,7 +5,12 @@
 #
 ########################################################################
 
-SUNTANSHOME=../../main
+module load mvapich2/2.1/gcc/4.4.7
+module load hdf5/1.8.6/gcc/4.4.7
+module load netcdf/4.1.3-v4/gcc/4.4.7
+
+SUNTANSHOME=$(pwd)
+SUNTANSHOME=$(echo ${SUNTANSHOME%/*}/main)
 SUN=$SUNTANSHOME/sun
 SUNPLOT=$SUNTANSHOME/sunplot
 
@@ -14,7 +19,7 @@ SUNPLOT=$SUNTANSHOME/sunplot
 maindatadir=rundata
 datadir=data
 
-NUMPROCS=$1
+NUMPROCS=15
 
 if [ -z "$MPIHOME" ] ; then
     EXEC=$SUN
@@ -25,17 +30,11 @@ fi
 if [ ! -d $datadir ] ; then
     cp -r $maindatadir $datadir
     echo Creating grid...
-    if [ -z "$TRIANGLEHOME" ] ; then
-	echo No triangle libraries installed.  
 	echo Copying points.dat, cells.dat, and edges.dat from $maindatadir to $datadir
 	$EXEC -g --datadir=$datadir
-    else
-	$EXEC -t -g --datadir=$datadir
-    fi
 else
     cp $maindatadir/suntans.dat $datadir/.
 fi
 
 echo Running suntans...
 $EXEC -s -vv --datadir=$datadir
-
