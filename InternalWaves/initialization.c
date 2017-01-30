c330b6e5ae1c6bfa63e6414b5fac86b36b5f1fda
Sorush Omidvar <sorush.omidvar@gmail.com>
2017-01-30 13:44:31 -0500

    Indentation has been done by Visual Studio for InternalWaves\Boundaries.c, InternalWaves\sources.c and InternalWaves\intialization.c

--- InternalWaves/initialization.c
+++ InternalWaves/initialization.c
@@ -12,46 +12,49 @@
  *
  */
 int GetDZ(REAL *dz, REAL depth, REAL localdepth, int Nkmax, int myproc) {
-  int k, status;
-  REAL z=0, dz0, r = GetValue(DATAFILE,"rstretch",&status);
+	int k, status;
+	REAL z = 0, dz0, r = GetValue(DATAFILE, "rstretch", &status);
 
-  if(dz!=NULL) {
-    if(r==1) 
-      for(k=0;k<Nkmax;k++)
-	dz[k]=depth/Nkmax;
-    else if(r>1 && r<=1.1) {    
-      dz[0] = depth*(r-1)/(pow(r,Nkmax)-1);
-      if(VERBOSE>2) printf("Minimum vertical grid spacing is %.2f\n",dz[0]);
-      for(k=1;k<Nkmax;k++) 
-	dz[k]=r*dz[k-1];
-    } else if(r>-1.1 && r<-1) {    
-      r=fabs(r);
-      dz[Nkmax-1] = depth*(r-1)/(pow(r,Nkmax)-1);
-      if(VERBOSE>2) printf("Minimum vertical grid spacing is %.2f\n",dz[Nkmax-1]);
-      for(k=Nkmax-2;k>=0;k--) 
-	dz[k]=r*dz[k+1];
-    } else {
-      printf("Error in GetDZ when trying to create vertical grid:\n");
-      printf("Absolute value of stretching parameter rstretch must  be in the range (1,1.1).\n");
-      exit(1);
-    }
-  } else {
-    r=fabs(r);
-    if(r!=1)
-      dz0 = depth*(r-1)/(pow(r,Nkmax)-1);
-    else
-      dz0 = depth/Nkmax;
-    z = dz0;
-    for(k=1;k<Nkmax;k++) {
-      dz0*=r;
-      z+=dz0;
-      if(z>=localdepth) {
-	return k;
-      }
-    }
-  }
+	if (dz != NULL) {
+		if (r == 1)
+			for (k = 0; k < Nkmax; k++)
+				dz[k] = depth / Nkmax;
+		else if (r > 1 && r <= 1.1) {
+			dz[0] = depth*(r - 1) / (pow(r, Nkmax) - 1);
+			if (VERBOSE > 2) printf("Minimum vertical grid spacing is %.2f\n", dz[0]);
+			for (k = 1; k < Nkmax; k++)
+				dz[k] = r*dz[k - 1];
+		}
+		else if (r > -1.1 && r < -1) {
+			r = fabs(r);
+			dz[Nkmax - 1] = depth*(r - 1) / (pow(r, Nkmax) - 1);
+			if (VERBOSE > 2) printf("Minimum vertical grid spacing is %.2f\n", dz[Nkmax - 1]);
+			for (k = Nkmax - 2; k >= 0; k--)
+				dz[k] = r*dz[k + 1];
+		}
+		else {
+			printf("Error in GetDZ when trying to create vertical grid:\n");
+			printf("Absolute value of stretching parameter rstretch must  be in the range (1,1.1).\n");
+			exit(1);
+		}
+	}
+	else {
+		r = fabs(r);
+		if (r != 1)
+			dz0 = depth*(r - 1) / (pow(r, Nkmax) - 1);
+		else
+			dz0 = depth / Nkmax;
+		z = dz0;
+		for (k = 1; k < Nkmax; k++) {
+			dz0 *= r;
+			z += dz0;
+			if (z >= localdepth) {
+				return k;
+			}
+		}
+	}
 }
-  
+
 /*
  * Function: ReturnDepth
  * Usage: grid->dv[n]=ReturnDepth(grid->xv[n],grid->yv[n]);
@@ -61,32 +64,32 @@ int GetDZ(REAL *dz, REAL depth, REAL localdepth, int Nkmax, int myproc) {
  *
  */
 REAL ReturnDepth(REAL x, REAL y) {
-  REAL Ls, xmid, Ds, D0;
+	REAL Ls, xmid, Ds, D0;
 
-  Ls = 30000;
-  xmid = 100000;
-  D0 = 3000;
-  Ds = 200;
-  return D0 - 0.5*(D0-Ds)*(1+tanh(4*(x-xmid)/Ls));
+	Ls = 30000;
+	xmid = 100000;
+	D0 = 3000;
+	Ds = 200;
+	return D0 - 0.5*(D0 - Ds)*(1 + tanh(4 * (x - xmid) / Ls));
 
-  if(x<=xmid-Ls/2)
-    return D0;
-  else if(x>xmid+Ls/2)
-    return Ds;
-  else
-    return D0-(D0-Ds)*((x-xmid)/Ls+0.5);
+	if (x <= xmid - Ls / 2)
+		return D0;
+	else if (x > xmid + Ls / 2)
+		return Ds;
+	else
+		return D0 - (D0 - Ds)*((x - xmid) / Ls + 0.5);
 }
 
- /*
-  * Function: ReturnFreeSurface
-  * Usage: grid->h[n]=ReturnFreeSurface(grid->xv[n],grid->yv[n]);
-  * -------------------------------------------------------------
-  * Helper function to create an initial free-surface. Used
-  * in phys.c in the InitializePhysicalVariables function.
-  *
-  */
+/*
+ * Function: ReturnFreeSurface
+ * Usage: grid->h[n]=ReturnFreeSurface(grid->xv[n],grid->yv[n]);
+ * -------------------------------------------------------------
+ * Helper function to create an initial free-surface. Used
+ * in phys.c in the InitializePhysicalVariables function.
+ *
+ */
 REAL ReturnFreeSurface(REAL x, REAL y, REAL d) {
-  return 0;
+	return 0;
 }
 
 /*
@@ -98,16 +101,16 @@ REAL ReturnFreeSurface(REAL x, REAL y, REAL d) {
  *
  */
 REAL ReturnSalinity(REAL x, REAL y, REAL z) {
-  REAL deltaS, alphaS, D_pycnocline;
+	REAL deltaS, alphaS, D_pycnocline;
 
-  deltaS = 0.024;
-  alphaS = 0.0187;
-  D_pycnocline = 20;
+	deltaS = 0.024;
+	alphaS = 0.0187;
+	D_pycnocline = 20;
 
-  if(z<-D_pycnocline)
-    return deltaS*pow(-z,0.0187);
-  else
-    return deltaS*pow(D_pycnocline,0.0187);
+	if (z < -D_pycnocline)
+		return deltaS*pow(-z, 0.0187);
+	else
+		return deltaS*pow(D_pycnocline, 0.0187);
 }
 
 /*
@@ -119,7 +122,7 @@ REAL ReturnSalinity(REAL x, REAL y, REAL z) {
  *
  */
 REAL ReturnTemperature(REAL x, REAL y, REAL z, REAL depth) {
-  return 1;
+	return 1;
 }
 
 /*
@@ -132,11 +135,11 @@ REAL ReturnTemperature(REAL x, REAL y, REAL z, REAL depth) {
  *
  */
 REAL ReturnHorizontalVelocity(REAL x, REAL y, REAL n1, REAL n2, REAL z) {
-  return 0;
+	return 0;
 }
 REAL ReturnSediment(REAL x, REAL y, REAL z, int sizeno) {
-  return 0;
+	return 0;
 }
-REAL ReturnBedSedimentRatio(REAL x, REAL y, int layer, int sizeno,int nsize) {
-   return 0;
+REAL ReturnBedSedimentRatio(REAL x, REAL y, int layer, int sizeno, int nsize) {
+	return 0;
 }
