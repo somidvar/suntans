7aa61829e10a99685a2d580661ff2e7d8dd72aac
Sorush Omidvar <sorush.omidvar@gmail.com>
2018-04-17 19:55:02 -0400

    Modifying the wind analyzor code, it works great

--- InternalWaves/matlabcode/WindAnalyzer.m
+++ InternalWaves/matlabcode/WindAnalyzer.m
@@ -2,95 +2,79 @@ close all;
 clear all;
 clc
 
-AvgPN=1;
-FileAddress='D:\OneDrive - University of Georgia\Documents\UGA Courses\Wind and Tide Study\2015NOAA.txt';
+AvgPN=6;
+FileAddress='D:\OneDrive - University of Georgia\Documents\UGA Courses\Wind and Tide Study\2017-BirdRock.txt';
 content = fileread(FileAddress) ;
-Data= textscan(content, '%d %d %d %d %d %d %f %f %f','HeaderLines', 1) ;
-WSNOAA=Data{8};
-WDNOAA=Data{9};
-TimeNOAA=Data{7};
+Data= textscan(content, '%f %f %f %f %f %f %f %f','HeaderLines', 1) ;
+WindSpeed=Data{4};
+WindDirection=Data{5};
+Time=Data{8};
+MonthBegins=[60,91,121,152,182,213];
+MonthEnds=[90,120,151,181,212,243];
 
-WDNOAA=-WDNOAA+270;%Converting the compass corrdinate system to polar coordinate system
-WDNOAA(WDNOAA>360)=WDNOAA(WDNOAA>360)-360;
-WDNOAA(WDNOAA<0)=WDNOAA(WDNOAA<0)+360;
-figure
-polarhistogram(WDNOAA(TimeNOAA>=180 & TimeNOAA<210)*pi()/180);
-%filtering the small wind
-WSNOAA(abs(WSNOAA)<0.5)=nan;
-WDNOAA(abs(WSNOAA)<0.5)=nan;
-TimeNOAA(abs(WSNOAA)<0.5)=nan;
-
-%Averaging the wind to get it hourly
-East=movmean(WSNOAA.*cosd(WDNOAA),AvgPN);
-North=movmean(WSNOAA.*sind(WDNOAA),AvgPN);
-TimeNOAA=movmean(TimeNOAA,AvgPN);
-%Recalculating the WDNOAA
-WDNOAA=atan2d(North,East);
-WDNOAA(WDNOAA>360)=WDNOAA(WDNOAA>360)-360;
-WDNOAA(WDNOAA<0)=WDNOAA(WDNOAA<0)+360;
-
-EastRotated=nan(size(WDNOAA,1),1);
-NorthRotated=nan(size(WDNOAA,1),1);
-fig=figure('units','normalized','outerposition',[0 0 1 1]);
-for MonthCounter=7:8
-    TimePeriodMin=30*(MonthCounter-1);
-    TimePeriodMax=30*MonthCounter;
-    TimePeriod=TimeNOAA(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax);
-    WDPeriod=WDNOAA(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax);
-    polarhistogram(WDPeriod*pi()/180);
-    
-    TempEast=East(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax);
-    TempNorth=North(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax);
-    RotationAngle=120;
-    C=[cosd(RotationAngle),sind(RotationAngle);-sind(RotationAngle),cosd(RotationAngle)];
-
-    sprintf(strcat('Rotation Angle=',num2str(RotationAngle)));
-    Temp=[TempEast,TempNorth]*C;
-    EastRotated(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax)=Temp(:,1);
-    NorthRotated(TimeNOAA>TimePeriodMin & TimeNOAA<TimePeriodMax)=Temp(:,2);
-    plot(TimePeriod,TempEast,'LineWidth',2);
-    hold on;
-    Divider=line([TimePeriod(1),TimePeriod(end)],[0,0]);
-    Divider.LineWidth=2;
-    Divider.Color='black';
-    grid minor;
-%     XTickLable=TimePeriodMin:5:TimePeriodMax;
-%     xticks(XTickLable);
-%     switch MonthCounter
-%         case 1
-%             str=strcat('Jan',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 2
-%             str=strcat('Feb',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 3
-%             str=strcat('Mar',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 4
-%             str=strcat('Apr',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 5
-%             str=strcat('May',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 6
-%             str=strcat('Jun',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 7
-%             str=strcat('Jul',{' '},num2str(YearNOAA(MonthCounter)));
-%         case 8
-%             str=strcat('Aug',{' '},num2str(YearNOAA(MonthCounter)));
-%     end
-%    title(str);
-
+f=figure('units','normalized','outerposition',[0 0 1 1]);
+for MonthCounter=3:8
+    subplot(2,3,MonthCounter-2);
+    polarhistogram(WindDirection(Time>=MonthBegins(MonthCounter-2) & Time<=MonthEnds(MonthCounter-2))*pi()/180);
+    str=strcat('Initial',{' '},num2str(MonthCounter),{' '},'2017');
+    title(str);
+    set(gca,'fontsize',16);
+    set(gca,'FontWeight','bold');
 end
-title('NOAA 9413450 Meteorological Gauge');
-xlabel('Day of the year');
-ylabel('Alongshore Wind Speed (m/s)');
+saveas(f,'InitialWind.png');
+
+%Converting the meteorological convention to triangular convetion, if wind
+%comes from south for meteo would be 180 and for tri 90. Meaning that it
+%!!!!GOES!!!! to 90 not come from 90
+%Averaging the wind to get it hourly
+WindDirection=-WindDirection+270;
+WindDirection(WindDirection>360)=WindDirection(WindDirection>360)-360;
+WindDirection(WindDirection<0)=WindDirection(WindDirection<0)+360;
+%Finding the East-West and South-North components
+East=movmean(WindSpeed.*cosd(WindDirection),AvgPN);
+North=movmean(WindSpeed.*sind(WindDirection),AvgPN);
+Time=movmean(Time,AvgPN);
+%Recalculating the WDNOAA
+WindDirection=atan2d(North,East);
+
+f=figure('units','normalized','outerposition',[0 0 1 1]);
+for MonthCounter=3:8
+    subplot(2,3,MonthCounter-2);
+    polarhistogram(WindDirection(Time>=MonthBegins(MonthCounter-2) & Time<=MonthEnds(MonthCounter-2))*pi()/180);
+    str=strcat('Processed',{' '},num2str(MonthCounter),{' '},'2017');
+    title(str);
+    set(gca,'fontsize',16);
+    set(gca,'FontWeight','bold');
+end
+saveas(f,'ProccessedWind.png');
+
+f=figure('units','normalized','outerposition',[0 0 1 1]);
+C=princaxes(East,North,1);
+xlabel('East-West');
+ylabel('North-South');
+str=sprintf('Max= %1.0f , Min= %1.0f & Elipse Ratio= %1.1f',atand(C(1)),atand(C(2)),C(3));
+title(str);
 set(gca,'fontsize',18);
 set(gca,'FontWeight','bold');
-hold off;
-saveas(fig,strcat('2017','NOAA','.png'));
+saveas(f,'PCA.png');
 
-fig=figure('units','normalized','outerposition',[0 0 1 1]);
-NorthRotated=Temp(:,2);
-EastRotated=Temp(:,1);
-Direction=atan(abs(NorthRotated./EastRotated));
-Direction(NorthRotated>0 & EastRotated>0)=0+Direction(NorthRotated>0 & EastRotated>0);
-Direction(NorthRotated>0 & EastRotated<0)=pi()-Direction(NorthRotated>0 & EastRotated<0);
-Direction(NorthRotated<0 & EastRotated<0)=pi()+Direction(NorthRotated<0 & EastRotated<0);
-Direction(NorthRotated<0 & EastRotated>0)=0-Direction(NorthRotated<0 & EastRotated>0);
-polarhistogram(Direction);
+%Now I want to rotate the wind to get the alongshore and cross-shore
+%components, to do so, I rotate it 120 degrees.
+C=[cosd(135),sind(135);-sind(135),cosd(135)];
+RotatedWind=[East,North]*C;
+EastRotated=RotatedWind(:,1);
+NorthRotated=RotatedWind(:,2);
+f=figure('units','normalized','outerposition',[0 0 1 1]);
+for MonthCounter=3:8
+    plot(Time(Time>=MonthBegins(MonthCounter-2) & Time<=MonthEnds(MonthCounter-2)),...
+        NorthRotated(Time>=MonthBegins(MonthCounter-2) & Time<=MonthEnds(MonthCounter-2)),'LineWidth',3);
+    hold on;
+end
+line([Time(1),Time(end)],[0,0],'Color','black','LineWidth',3);
+xlabel('Day of Year');
+ylabel('Wind Speed (m/s)');
+title('Alongshore(SW) Wind Speed');
+set(gca,'fontsize',16);
+set(gca,'FontWeight','bold');
+grid minor;
+saveas(f,'AlongshoreWind.png');
