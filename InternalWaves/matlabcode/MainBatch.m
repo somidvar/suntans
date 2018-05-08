b62cc6b943ed6e9aaafc66677f95b0778e9c68db
Sorush Omidvar <sorush.omidvar@gmail.com>
2018-05-08 19:27:06 -0400

    Modifying the MainBatch to be compatible with series 90000

--- InternalWaves/matlabcode/MainBatch.m
+++ InternalWaves/matlabcode/MainBatch.m
@@ -1,10 +1,11 @@
 %This code is plotting the SUNTANS results for several case scenarios with
 %similar names. This code is written by Sorush Omidvar in July 2016 at UGA.
 %This vesion is modified in Aug 2016.
-for CaseNumber=70006:1:70047
+for CaseNumber=90402:1:90587
     clearvars -except CaseNumber;
     close all;
     clc;
+    SerieVersion=90000;
     
     format compact;
     disp(strcat('Case Number= ',num2str(CaseNumber)))
@@ -23,58 +24,70 @@ for CaseNumber=70006:1:70047
         DataPath=strcat(Prefix,CaseName);
         DataPath=strcat(DataPath,'/InternalWaves/data/Result_0000.nc');
     else
-        Prefix='F:\9th\suntans-9th-';
-        OutputAddress='D:\9th\';
+        Prefix='D:\suntans-9th-';
+        OutputAddress='D:\';
         CaseName=num2str(CaseNumber);
         DataPath=strcat(Prefix,CaseName);
         DataPath=strcat(DataPath,'\InternalWaves\data\Result_0000.nc');
     end
-
+    
     if exist(DataPath,'file')==0
 		disp('There is no NETCDF file to process')
         continue;
     end
     
     KnuH=1;
-    g=9.8;  
     KappaH=0;
-    DragCoeff=0.001;
-    InterpolationEnhancement=10;%Resolution of interpolation in energy flux calculation 
-    XEndIndex=2000;% The data trim after this X point
-    TimeStartIndex=5888/2;%This is 27 cycles of M2 and 14 cycles of K1 and wind
-    TimeEndIndex=floor(9917/2);%This is 27 cycles of M2 and 14 cycles of K1 and wind
-    
-    AnalysisSpeed=2;
+    g=9.8;
+    InterpRes=5;%Resolution of interpolation in energy flux calculation 
+    XEndIndex=Inf;% The data trim after this X point
+    TimeStartIndex=1009;%This is 27 cycles of M2 and 14 cycles of K1 and wind
+    TimeEndIndex=Inf;%This is 27 cycles of M2 and 14 cycles of K1 and wind
+
+    AnalysisSpeed=1;
     FPSMovie=15;
     
     %Setting the wind frequency based on the cases
     WindOmega=2*pi/(24*3600);
     
     %Setting the wind stress based on the cases
-    if floor((CaseNumber-70000)/12)==0
+    if floor(mod((CaseNumber-SerieVersion),196)/28)==0
         WindTauMax=0e-5;
-    elseif floor((CaseNumber-70000)/12)==1
-        WindTauMax=2.5e-5;
-    elseif floor((CaseNumber-70000)/12)==2
-        WindTauMax=5e-5;
-    elseif floor((CaseNumber-70000)/12)==3
-        WindTauMax=7.5e-5;
+    else
+        WindTauMax=1;
     end       
     %Setting the dirunal and semi-dirunal tide frequency based on the cases
-    if mod(CaseNumber,4)==0
+    if mod(CaseNumber-SerieVersion,4)==0
         DiurnalTideOmega=0;
         SemiDiurnalTideOmega=0;
-    elseif mod(CaseNumber,4)==1
+    elseif mod(CaseNumber-SerieVersion,4)==1
         DiurnalTideOmega=2*pi/(23.93*3600);
         SemiDiurnalTideOmega=0;
-    elseif mod(CaseNumber,4)==2
+    elseif mod(CaseNumber-SerieVersion,4)==2
         DiurnalTideOmega=0;
         SemiDiurnalTideOmega=2*pi/(12.42*3600);
-    elseif mod(CaseNumber,4)==3
+    elseif mod(CaseNumber-SerieVersion,4)==3
         DiurnalTideOmega=2*pi/(23.93*3600);
         SemiDiurnalTideOmega=2*pi/(12.42*3600);        
     end
-    WavePlotter(AnalysisSpeed,FPSMovie,DiurnalTideOmega,SemiDiurnalTideOmega,WindOmega,WindTauMax*1e4,DataPath,OutputAddress,num2str(CaseNumber));
+    
+    if floor((CaseNumber-SerieVersion)/196)==0
+        ModelTimeOffset=-1;
+        WindLag=15;
+    elseif floor((CaseNumber-SerieVersion)/196)==1
+        ModelTimeOffset=2;
+        WindLag=18;
+    elseif floor((CaseNumber-SerieVersion)/196)==2
+        ModelTimeOffset=5;
+        WindLag=21;
+    elseif floor((CaseNumber-SerieVersion)/196)==3
+        ModelTimeOffset=8;
+        WindLag=24;
+    elseif floor((CaseNumber-SerieVersion)/196)==4
+        ModelTimeOffset=11;
+        WindLag=27;
+    end
+    WavePlotter(AnalysisSpeed,FPSMovie,DiurnalTideOmega,SemiDiurnalTideOmega,WindOmega,WindTauMax,DataPath,OutputAddress,num2str(CaseNumber),ModelTimeOffset,WindLag);
 %     EnergyFluxCalculator(DataPath,CaseNumber,OutputAddress,...
 %         KnuH,KappaH,g,InterpolationEnhancement,XEndIndex,...
 %         DiurnalTideOmega,SemiDiurnalTideOmega,WindTauMax,TimeStartIndex,...
